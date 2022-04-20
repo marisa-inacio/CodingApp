32cd1bf4468778a338b190edaa747c16ee3a696b
Marisa Inacio <marisa.vicente.inacio@hotmail.com>
2022-04-20 18:03:19 +0100

    CodingAPP FirstCommit

---
+++ CodingDecodingDLL/CodingDecoding.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Text;
+
+namespace CodingDecodingDLL
+{
+
+    //video de apoio para a criação e referenciação da Class Library_https://www.youtube.com/watch?v=jtZDub6xzW4
+    
+    public class CodingDecoding
+    {
+
+            public static string Base64Encode(string text)
+            {
+
+                return Convert.ToBase64String(Encoding.UTF8.GetBytes(text.ToString()));
+            }
+
+            public static string Base64Decode(string text)
+            {
+                return Encoding.UTF8.GetString(Convert.FromBase64String(text.ToString()));
+            }
+        
+    }
+}
